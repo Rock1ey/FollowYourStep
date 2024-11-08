d38564471c8012d4f61322b5322168827b7731c7
Rock1ey <2061218098@qq.com>
2024-11-08 11:39:52 +0800

    Initial Commit

---
+++ FollowYourStep/FollowYourStepApp.swift
@@ -0,0 +1,17 @@
+//
+//  FollowYourStepApp.swift
+//  FollowYourStep
+//
+//  Created by admin on 2024/11/8.
+//
+
+import SwiftUI
+
+@main
+struct FollowYourStepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
